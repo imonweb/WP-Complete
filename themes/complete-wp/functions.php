8cd8053dd4bdc09d36484c1b659980fa29020b23
Imon <contact@imonweb.co.uk>
2023-01-05 10:00:30 +0000

    enqueue files

---
+++ themes/complete-wp/functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+/*  Variables */
+
+/*  Includes */
+include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/front/head.php'));
+
+/*  Hooks */
+add_action( 'wp_enqueue_scripts', 'u_enqueue' );
+add_action('wp_head', 'u_head', 5);
