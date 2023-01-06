580fb59735705fb0b5db6e39ffa042596c6341d5
Imon <contact@imonweb.co.uk>
2023-01-06 00:01:42 +0000

    complete plus plugin created

---
+++ plugins/complete-plus/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Complete Plus
+ * Plugin URI:        https://github.com/imonweb/WP-Complete.git
+ * Description:       A plugin for adding blocks to a theme. 
+ * Version:           0.0.1
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Imon
+ * Author URI:        http://imonweb.co.uk
+ * Text Domain:       completeplus
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/imonweb/WP-Complete.git
+ */
