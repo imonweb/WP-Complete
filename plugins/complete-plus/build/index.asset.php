96fe588f39c95596c80e9317f196af71f28cca9b
Imon <contact@imonweb.co.uk>
2023-01-06 01:10:34 +0000

    block plugin

---
+++ plugins/complete-plus/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ba6709340ba73ff1a74a');
--- plugins/complete-plus/index.php
+++ plugins/complete-plus/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       Complete Plus
- * Plugin URI:        https://github.com/imonweb/WP-Complete.git
+ * Plugin URI:        https://github.com/imonweb/WP-Complete/tree/master/plugins/complete-plus
  * Description:       A plugin for adding blocks to a theme. 
  * Version:           0.0.1
  * Requires at least: 5.9
@@ -12,5 +12,10 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/imonweb/WP-Complete.git
+ * GitHub Plugin URI: https://github.com/imonweb/WP-Complete/tree/master/plugins/complete-plus
  */
+
+if(!function_exists('add_action')){
+  echo 'Seems like you stumbled here by accident.';
+  exit;
+}
